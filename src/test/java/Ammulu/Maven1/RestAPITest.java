a21df6bc716f566f9b4c0e9e6815a55efbf8b447
AmmuBhavi <annambhavya1506@gmail.com>
2022-08-03 13:23:12 +0530

    first commit

---
+++ src/test/java/Ammulu/Maven1/RestAPITest.java
@@ -0,0 +1,17 @@
+package Ammulu.Maven1;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void Postjira()
+	{
+		System.out.println("Postjira");
+	}
+	@Test
+	public void Deletedtwitter()
+	{
+		System.out.println("Deletedtwitter");
+	}
+}
---
+++ src/test/java/Ammulu/Maven1/SeleniumTest.java
@@ -0,0 +1,17 @@
+package Ammulu.Maven1;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void Browserautomtion()
+	{
+		System.out.println("Browserautomtion");
+	}
+	@Test
+	public void Elementsui()
+	{
+		System.out.println("Elementsui");
+	}
+}
